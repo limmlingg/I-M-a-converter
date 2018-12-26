13c7f9048f53b369891ab368d0c8e06dcea75a4e
Miao Ling <limmlingg@gmail.com>
2018-12-26 17:25:55 +0800

    Fixed 1 bug, a lot more appeared
    Added: Ability to detect and parse negative numbers, more logging
    Bugs remaining: Unable to replace text which has partial styling, e.g. "<b>70</b>°F"
    New bugs: Replacing HTML sometimes causes the f2ctag to be added multiple times with strange titles. Sometimes, the numbers will have spaces be inserted randomly, e.g. "200°F" becomes "20 0°F", this will sometimes be parsed as 200°F and sometimes as 0°F.

--- conversion.js
+++ conversion.js
@@ -12,55 +12,87 @@ var pageText;
 // Get the entire text of the webpage
 function getText() {
 	pageText = document.body.innerText;
-	console.log(pageText);
+	//console.log(pageText);
 }
 
 // Look through the entire text of the webpage and add title attributes with the celcius units
 // In the future, if extending this plugin, each unit conversion should have its own method
 function addFahrenheitTitles() {
 	getText();
-	// Search for at least 1 digit, an optional space and at least one digit after, an optional space, and the fahrenheit unit
+	// Search for an optional negate sign, at least 1 digit, an optional space and at least one digit after, an optional space, and the fahrenheit unit
 	// /g to search for all matches
-	//var fahreinheitRegex = /\d+(.\d+)?(\s)?°F/g;
-	var matches = pageText.match(/\d+(.\d+)?(\s)?°F/g);
+	var matches = pageText.match(/[−|-]?\d+(\.\d+)?[\s]?°F/g);
 
-	if (matches == null) {
-		console.log("didn't find anything");
-	}
-
-	console.log(matches[1]);
+	// if (matches == null) {
+	// 	console.log("didn't find anything");
+	// }
 
 	// Remove all duplicates so that replacing the text in setTitle() will not cause multiple spans for a duplicate
-	//for (occurrence : matches.filter(onlyUnique)) {
+	matches = Array.from(new Set(matches));
+
 	for (var i = 0; i < matches.length; i++) {
-		console.log("match " + i + ": " + matches[i]);
+		//console.log("match " + i + ": " + matches[i]);
 
 		var occurrence = matches[i];
-
 		var fahrenheit = extractFahrenheitNum(occurrence);
-		var celsius = fahrenheitToCelsius(fahrenheit);
-		setTitle(occurrence, makeFahrenheitTitle(fahrenheit, celsius));
+
+		if (fahrenheit != null) {
+			var celsius = fahrenheitToCelsius(fahrenheit);
+			setTitle(occurrence, makeFahrenheitTitle(fahrenheit, celsius));
+		}
 	}
 }
 
 // Given a string with 1 fahrenheit value (including units), returns the number 
 function extractFahrenheitNum(str) {
-	var num = str.match(/\d+(.\d+)?/g);
+	var num = str.match(/[−|-]?\d+(\.\d+)?/g);
+
+	if (num == null) {
+		console.log("Error: could not parse number for string \"" + str + "\"");
+	}
+
 	if (num.length > 1) {
-		console.log("Error: more than one numeric part found for text \"" + occurence + "\"");
+		console.log("Error: more than one numeric part found for text \"" + str + "\"");
 		return null;
 	}
-	return parseFloat(num[0]);
+
+	// Replace the negative sign with a hyphen so that parseFloat() can parse it as a negative number
+	num[0] = num[0].replace("−", "-");
+
+	var floatNum = parseFloat(num[0]);
+
+	if (isNaN(floatNum)) {
+		console.log("Error: could not parse float for " + num[0]);
+		return null;
+	} else {
+		return parseFloat(num[0]);
+	}
 }
 
 function fahrenheitToCelsius(fahrenheit) {
-	return (fahrenheit - 32) * 5.0/9.0;
+	var celsius = parseFloat((fahrenheit - 32) * 5.0/9.0);
+
+	if (isNaN(celsius)) {
+		console.log("Error: non numeric celsius calculated for " + fahrenheit + " fahrenheit. Celcius = " + celsius);
+		return null;
+	} else {
+		return celsius;
+	}
 }
 
 function makeFahrenheitTitle(fahrenheit, celsius) {
-	return fahrenheit + "°F = " + celsius.toFixed(2) + "°C"
+	return String(fahrenheit) + "°F = " + String(celsius.toFixed(2)) + "°C"
 }
 
 function setTitle(original, title) {
-	document.body.innerHTML = document.body.innerHTML.replace(original, "<span title=\"" + title + "\"> " + original + "</span>")
+
+	//console.log(title);
+	//console.log(original);
+	//console.log("<f2ctag style=\"border-bottom: 2px solid #fea\" title=\"" + title + "\"> " + original + "</f2ctag>");
+
+	document.body.innerHTML = document.body.innerHTML.replace(new RegExp(original, "g"), "<f2ctag style=\"border-bottom: 2px solid #fea\" title=\"" + title + "\"> " + original + "</f2ctag>");
+
+	//$("body:contains(original)").html(function(_, html) {
+	//	return html.replace(original, "<f2ctag style=\"border-bottom: 2px solid #fea\" title=\"" + title + "\"> " + $1 + "</f2ctag>")
+	//})
 }
